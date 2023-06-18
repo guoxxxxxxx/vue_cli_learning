e9a6cdc2e39e8c07c9f422c3895b3f0ac49077a0
guo_x <guo_x0315@163.com>
2023-06-18 21:19:09 +0800

    分析脚手架

---
+++ 01_src_分析脚手架/components/School.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称: {{schoolName}}</h2> <br>
+        <h2>学校地址: {{address}}</h2>
+        <button @click="showInfo">点我提示信息</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互的相关代码 
+    export default {
+        name: 'School',
+        data() {
+            return {
+                schoolName: 'NCEPU',
+                address: 'BeiJing'
+            }
+        },
+        methods: {
+            showInfo(){
+                alert(this.schoolName);
+            }
+        },
+    }
+
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo {
+        background-color: aqua;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生名称: {{name}}</h2> <br>
+        <h2>学生年龄: {{age}}</h2>
+        <button @click="showInfo">点我提示信息</button>
+    </div>
+</template>
+
+<script>
+    // 组件交互的相关代码 
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: 'Tim',
+                age: 23
+            }
+        },
+        methods: {
+            showInfo(){
+                alert(this.name);
+            }
+        },
+    }
+
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo {
+        background-color: aqua;
+    }
+</style>
