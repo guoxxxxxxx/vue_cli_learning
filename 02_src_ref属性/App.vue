190366e13235bba83b783b56176d556fe9ba5b10
guo_x <guo_x0315@163.com>
2023-06-19 08:41:57 +0800

    ref属性

---
+++ 02_src_ref属性/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+// 引入school组件
+import School from './components/School';
+
+export default {
+    name: 'App',
+    components:{ School }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 02_src_ref属性/components/School.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="showInfo">点我</button>
+    <h2 ref="sname">学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: 'NCEPU',
+            address: 'BeiJing'
+        }
+    },
+    methods:{
+        showInfo(){
+            console.log(this.$refs.sname);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
