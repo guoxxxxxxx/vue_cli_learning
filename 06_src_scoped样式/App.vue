f75494ba545e0a45ec608453e07cee8b05fdd5f2
guo_x <guo_x0315@163.com>
2023-06-19 10:24:54 +0800

    scoped 样式

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- id默认传的是字符串，加上: 则说明是数字 -->
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入school组件
+import School from './components/School';
+import Student from './components/Student.vue'
+
+export default {
+    name: 'App',
+    components:{ School, Student }
+}
+</script>
+
+<style>
+
+</style>
