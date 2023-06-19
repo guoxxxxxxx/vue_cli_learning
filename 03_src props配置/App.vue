43262dc24dca9a45306a7becd60cc6bdd7010a8d
guo_x <guo_x0315@163.com>
2023-06-19 09:03:21 +0800

    props配置

---
+++ 03_src props配置/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- id默认传的是字符串，加上: 则说明是数字 -->
+    <School name="张三" address="BeiJing" :id="1"></School>
+  </div>
+</template>
+
+<script>
+// 引入school组件
+import School from './components/School';
+
+export default {
+    name: 'App',
+    components:{ School }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 03_src props配置/components/School.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="showInfo">点我</button>
+    <h2 ref="sname">学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+    <h2>学校id: {{ id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            msg: 'welcome to beijing!'
+        }
+    },
+    methods:{
+        showInfo(){
+            console.log(this.$refs.sname);
+        }
+    },
+    // props:['name', 'address', 'id'] // 简单接收
+
+    // 接收的同时对数据进行类型限制
+    /*
+    props:{
+        name: String,
+        id: Number,
+        address: String
+    }
+    */
+    /**
+     * 不要修改传进来的数值，如果非要改的话，可以再data里面定义新的数据，存储接收来的数据，修改data中的数据
+     */
+    props:{
+        name:{
+            type: String,   // 类型
+            require: true,  //是否一定需要
+        },
+        id:{
+            type: Number,
+            default: 100,   // 默认值
+        },
+        address:{
+            type: String
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
