a98009c206fdea15ba4c95434fc14eb9bbf5b23a
guo_x <guo_x0315@163.com>
2023-06-19 09:15:17 +0800

    mixin混合

---
+++ 04_src_mixin混合/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2 @click="showName">name: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+  </div>
+</template>
+
+<script>
+
+// 引入一个mixin
+import {mixin} from '../mixin'
+
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: 'Ncepu',
+            age: 66
+        }
+    },
+    mixins:[mixin]
+}
+</script>
+
+<style>
+
+</style>
