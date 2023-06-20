2d8e50a7247e12596c07a541eaa470be87b9f4c8
guo_x <guo_x0315@163.com>
2023-06-20 09:23:32 +0800

    组件间消息传递

---
+++ 10_src_自定义组件(消息传递)/components/School.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>name: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+    <button @click="sendSchoolName">把学校的名称给APP</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: 'Ncepu',
+            age: 66
+        }
+    },
+    props:['getSchoolName'],
+    methods:{
+      sendSchoolName(){
+        this.getSchoolName(this.name);
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
