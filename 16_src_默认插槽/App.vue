784c7b6071b218fd097c371719f7ddec333a1ea1
guo_x <guo_x0315@163.com>
2023-06-21 11:01:40 +0800

    默认插槽

---
+++ 16_src_默认插槽/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <category :title="'美食'" :listData="foods">
+      <img src="https://www.google.com/favicon.ico" alt="" />
+    </category>
+    <category :title="'游戏'">
+      <ul>
+        <li v-for="(item, index) in games" :key="index">{{ item }}</li>
+      </ul>
+    </category>
+    <category :title="'电影'">
+      <ul>
+        <li v-for="(item, index) in films" :key="index">{{ item }}</li>
+      </ul>
+    </category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+export default {
+  components: { Category },
+  data() {
+    return {
+      foods: ["火锅", "小龙虾", "烧烤", "牛排"],
+      games: ["红警", "CF", "劲舞团", "超级玛丽"],
+      films: [
+        "《教父》",
+        "《拆弹专家》",
+        "《你好，李焕英》",
+        "《我的兄弟叫顺溜》",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ 16_src_默认插槽/components/Category.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="category">
+    <h3>{{title}}</h3>
+    <slot>我是一些默认值，当使用者没有传递具体结构时，我会出现！</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props:['title']
+}
+</script>
+
+<style>
+.category {
+    background-color: aqua;
+    width: 200px;
+    height: 300px;
+}
+</style>
