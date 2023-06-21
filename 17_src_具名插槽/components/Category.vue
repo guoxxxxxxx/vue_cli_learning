f2e7a6b9006e7a4f0de7c8c3c71a8379c312af04
guo_x <guo_x0315@163.com>
2023-06-21 11:52:35 +0800

    具名插槽

---
+++ 17_src_具名插槽/components/Category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="category">
+    <h3>{{title}}</h3>
+    <slot name="header">我是一些默认值，当使用者没有传递具体结构时，我会出现！</slot>
+    <slot name="footer">我是一些默认值，当使用者没有传递具体结构时，我会出现！</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props:['title']
+}
+</script>
+
+<style>
+.category {
+    background-color: aqua;
+    width: 200px;
+    height: 300px;
+}
+</style>
