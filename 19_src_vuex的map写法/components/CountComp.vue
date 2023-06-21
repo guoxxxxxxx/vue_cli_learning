cd6ca04308a335298b6e8df1f358dc62c4f1d5b3
guo_x <guo_x0315@163.com>
2023-06-21 22:11:22 +0800

    map写法

---
+++ 19_src_vuex的map写法/components/CountComp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>当前求和为: {{$store.state.sum}}</h1>
+    <h1>当前求和的10倍为: {{$store.getters.bigSum}}</h1>
+    <select v-model.number="n">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+    </select>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementOdd">当前求和为奇数再加</button>
+    <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    name: 'CountComp',
+    data() {
+        return {
+            n: 1,
+        }
+    },
+    methods:{
+        increment(){
+            this.$store.dispatch('add', this.n)
+            // 如果不向后端请求数据可以直接调用commit
+            // this.$store.commit('ADD', this.n)
+        },
+        decrement(){
+
+        },
+        incrementOdd(){
+
+        },
+        incrementWait(){
+
+        }
+    },
+    computed:{
+        // 借助mapState生成计算属性，从state中读取数据
+        ...mapState({sum: 'sum'}),
+
+        // 数组写法
+        // ...mapState(['sum'])
+    },
+    mounted(){
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 19_src_vuex的map写法/store/index.js
@@ -0,0 +1,37 @@
+// 该文件用于创建Vuex中的Store
+import Vue from 'vue'
+//引入Vuex
+import Vuex from 'vuex'
+
+// 准备actions 用于响应组件中的动作 (方法一般都小写)
+const actions = {
+    add(context, value){
+        context.commit('ADD', value)
+    }
+}
+// 准备mutations 用于操作数据 (方法一般都全部大写)
+const mutations = {
+    ADD(state, value){
+        state.sum += value;
+    }
+}
+// 准备state用于存储数据
+const state = {
+    sum:0,  // 当前的和
+}
+
+// 用于将state中的数据进行加工
+const getters = {
+    bigSum(state){
+        return state.sum * 10
+    }
+}
+
+Vue.use(Vuex)
+// 创建store 并暴露
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state,
+    getters,
+})
