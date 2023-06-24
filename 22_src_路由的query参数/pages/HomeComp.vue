d625b9e71a26fa255967d563063615336261f896
guo_x <guo_x0315@163.com>
2023-06-24 19:50:53 +0800

    路由的query参数

---
+++ 22_src_路由的query参数/pages/HomeComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+        <router-link to="/home/news" active-class="active">News</router-link>
+      </li>
+      <li>
+        <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+        <router-link to="/home/message" active-class="active">Message</router-link>
+      </li>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeComp",
+};
+</script>
+
+<style>
+</style>
---
+++ 22_src_路由的query参数/router/index.js
@@ -0,0 +1,39 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from "vue-router";
+
+// 引入组件
+import AboutComp from '../pages/AboutComp'
+import HomeComp from '../pages/HomeComp'
+import NewsComp from '../pages/NewsComp'
+import MessageComp from '../pages/MessageComp'
+import DetailComp from '../pages/DetailComp'
+
+// 创建一个路由器并暴露
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: AboutComp
+        },
+        {
+            path:'/home',
+            component: HomeComp,
+            children: [
+                {
+                    path: 'news',
+                    component: NewsComp
+                },
+                {
+                    path: 'message',
+                    component: MessageComp,
+                    children: [                        
+                        {
+                            path: 'detail',
+                            component: DetailComp
+                        }                        
+                    ]
+                }
+            ]
+        }
+    ]
+})
