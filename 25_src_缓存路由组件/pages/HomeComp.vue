2c5f136e57c338aa8b150da86d63906fe6391b46
guo_x <guo_x0315@163.com>
2023-06-25 13:16:12 +0800

    缓存路由组件

---
+++ 25_src_缓存路由组件/pages/HomeComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>我是Home的内容</h2>
+    <ul class="nav nav-tabs">
+      <li>
+        <!-- <a class="list-group-item" href="./home-news.html">News</a> -->
+        <router-link to="/home/news" active-class="active">News</router-link>
+      </li>
+      <li>
+        <!-- <a class="list-group-item active" href="./home-message.html">Message</a> -->
+        <router-link to="/home/message" active-class="active"
+          >Message</router-link
+        >
+      </li>
+    </ul>
+    <keep-alive include="NewsComp">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeComp",
+};
+</script>
+
+<style>
+</style>
